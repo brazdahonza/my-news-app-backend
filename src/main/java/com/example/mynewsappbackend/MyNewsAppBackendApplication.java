30554a912767b681e78406786868ab74be644ee8
Jan Brazda <brazdahonza@gmail.com>
2024-05-31 18:49:32 +0200

    Initial commit

---
+++ src/main/java/com/example/mynewsappbackend/MyNewsAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.mynewsappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyNewsAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyNewsAppBackendApplication.class, args);
+    }
+
+}
